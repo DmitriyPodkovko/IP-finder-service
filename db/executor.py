bcbac9a0ea09fccffcfcc32f6949e85224e11bb4
DmitriyPodkovko <dapodkovko@gmail.com>
2025-02-20 10:58:14 +0200

    Add close all connections

--- db/executor.py
+++ db/executor.py
@@ -43,7 +43,10 @@ class DBExecutor:
 
     def connect_off(self) -> None:
         try:
-            self._cursor.close()
+            if self._cursor:
+                self._cursor.close()
+            connections['filter'].close()
+            connections.close_all()
             print(f'CONNECT OFF')
             logging.info(f'CONNECT OFF')
         except Exception as e:
